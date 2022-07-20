e5c9b87086ff87e038059cc3651b351206cf036b
hs <7heesu7@hanmail.net>
2022-07-20 14:00:53 +0900

    thymeleaf

--- jmt/src/main/java/com/java/jmt/HomeController.java
+++ jmt/src/main/java/com/java/jmt/HomeController.java
@@ -1,14 +1,21 @@
 package com.java.jmt;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
-    @RequestMapping(value = "/home")
+    @RequestMapping("/home")
     public String home(){
-        return "home.html";
+        return "home";
+    }
+
+    @RequestMapping("/test")
+    public String test(Model model){
+        model.addAttribute("testm","테스트메시지입니다");
+        return "test";
     }
 
     @ResponseBody
